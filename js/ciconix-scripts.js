5fd1ac3a2f9857d6b568c1b92f23ea7001d7a200
childressdev <jcampbell@childressagency.com>
2018-03-21 19:07:51 -0400

    newsletter demo styled

--- js/ciconix-scripts.js
+++ js/ciconix-scripts.js
@@ -1,14 +1,19 @@
 jQuery(document).ready(function($){
-  $('.member-bio').on('show..bs.collapse', function(){
+  $('.member-bio').on('show.bs.collapse', function(){
     var member = $(this).attr('id');
     $('.in').each(function(){
       $(this).collapse('hide');
     });
 
+    //$('.member-image').each(function(){
+    //  $(this).removeClass('shadow-link');
+    //});
+
+    $('[data-target=".' + member + '"]').addClass('shadow-link');
+  });
+  $('.member-bio').on('hide.bs.collapse', function(){
     $('.member-image').each(function(){
       $(this).removeClass('shadow-link');
     });
-
-    $('[data-target=".' + member + '"]').addClass('shadow-link');
   });
 });
